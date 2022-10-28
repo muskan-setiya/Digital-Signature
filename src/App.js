aad8e76ec3f5e2fc55298de1af3b1f73679d90cf
muskan-setiya <muskansetiya1706@gmail.com>
2022-10-29 01:50:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+import { ethers } from 'ethers';
 import './App.css';
 
 function App() {
+
+  const init = async() => {
+    console.log("working..");  
+
+    //create wallet
+    const wallet = ethers.Wallet.createRandom();
+    console.log({wallet});
+
+    //message to be signed
+    const msg1 = "muskan";
+    const msg2 = "muskan";  
+
+    //Signing the message; making it a signature
+    const signatureHash1 = await wallet.signMessage(msg1);
+    console.log("Signature Hash of Msg1 : " ,{ signatureHash1 });
+
+    const signatureHash2 = await wallet.signMessage(msg2);
+    console.log("Signature Hash of Msg2 : " ,{ signatureHash2 });
+
+    //split signatureHash
+    const signature1 = ethers.utils.splitSignature(signatureHash1);
+    console.log( "signature 1 : ",{signature1});
+
+    const signature2 = ethers.utils.splitSignature(signatureHash2);
+    console.log( "signature 2 : ",{signature2});
+
+    //verify the address of the signature and message so that it matches the originating wallet signer 
+    const verifiedAddress1 = await ethers.utils.verifyMessage(msg1 , signature1);
+    const verifiedAddress2 = await ethers.utils.verifyMessage(msg2 , signature2);
+
+    //ethers.utils.verifyMessage : 
+    //   Returns the address of the account that signed messageStringOrArrayish to generate signature.
+    console.log({verifiedAddress1});
+    console.log({verifiedAddress2});
+
+
+    console.log({ isVerified : verifiedAddress1 === wallet.address});
+    console.log({ isVerified : verifiedAddress2 === wallet.address});
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={init}>Create Random Wallet</button>
       </header>
     </div>
   );
